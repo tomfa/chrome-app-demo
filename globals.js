655a9bc97b0bfaade049169e9ef86650dca306cf
Tomas Fagerbekk <tomas@webutvikling.org>
2018-10-09 22:55:36 +0200

    Add chrome extension dummy app

---
+++ globals.js
@@ -0,0 +1,14 @@
+// Mandatory to make app work
+window.applicationKey = null;
+
+/* Optional to set if you wish to use your own boards
+* Find by adding /reports.json to your trello board url
+*/
+window.boardId = null;
+window.listId = null;
+window.doneListId = null;
+
+/* Sandbox key
+* You can create your own at https://developers.trello.com/page/sandbox
+*/
+window.token = null;
